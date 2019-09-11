d9e31ac71536ac3586d45651259edd79046f6e71
lpavonUTC <luispavon.utc@gmail.com>
2019-09-11 06:46:49 -0500

    insumos

---
+++ src/java/controlador/controladorInsumos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import javax.servlet.http.HttpServlet;
+import modelo.Insumo;
+/**
+ *
+ * @author Luis Pavón
+ */
+public class controladorInsumos extends HttpServlet{
+    
+    String listar = "vistas/insumo/listar.jsp";
+    
+}
